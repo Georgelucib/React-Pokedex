2c674dae96ece66595c7089e57f926ea33fe6fb5
George Barros <georgelucib@gmail.com>
2022-11-09 02:55:06 -0300

    Updates

---
+++ precache-manifest.941e8c3984dbc626617bb023c9c19619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99d7520c514c39d5dbd4",
+    "url": "/React-Pokedex/static/css/main.6d459e83.chunk.css"
+  },
+  {
+    "revision": "99d7520c514c39d5dbd4",
+    "url": "/React-Pokedex/static/js/main.99d7520c.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/React-Pokedex/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "abb25312b4b0ba0da109",
+    "url": "/React-Pokedex/static/js/2.abb25312.chunk.js"
+  },
+  {
+    "revision": "071c960b071e3ef25c0b",
+    "url": "/React-Pokedex/static/js/runtime~main.071c960b.js"
+  },
+  {
+    "revision": "e8ae69fa0a47fa7ddf026a667c299441",
+    "url": "/React-Pokedex/index.html"
+  }
+];
